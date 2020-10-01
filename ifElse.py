b1d00359b75104a7f95f1eb27e680f2496867912
Pranav <72198941+pranav102001@users.noreply.github.com>
2020-10-01 19:23:00 +0530

    Added Python Implementation File of IF else ladder

---
+++ ifElse.py
@@ -0,0 +1,10 @@
+Age = input("Input your age!")
+
+if Age == 18 && Age < 25:
+    print("You are not a Teenager!")
+    
+elif Age < 18:
+    print("You are still a kid!")
+    
+else Age > 25:
+    print("You are to old to be a kid!")
